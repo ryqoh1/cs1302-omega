29f47ed0f85252862c68d60c09e14a3957fd550a
Nhan Thanh Tran <ntt21786@csci-odin.cs.uga.edu>
2022-11-30 19:44:19 -0500

    refactored main menu initialization

--- src/main/java/cs1302/omega/OmegaApp.java
+++ src/main/java/cs1302/omega/OmegaApp.java
@@ -18,78 +18,89 @@ import javafx.stage.Stage;
  */
 public class OmegaApp extends Application {
 	
+	static final int SCENE_WIDTH = 680;
+	static final int SCENE_HEIGHT = 640;
+	
+	static final Font F40 = Font.font("C059", FontWeight.MEDIUM, 40);
+	
+	Scene mainMenu;
+	
 	/**
 	 * Constructs an {@code OmegaApp} object. This default (i.e., no argument)
 	 * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
 	 */
 	public OmegaApp() {
 	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public void init() {
+
+	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void start(Stage stage) {
 
-		VBox root = new VBox();
-		root.setAlignment(Pos.CENTER);
-		Font f1 = Font.font("C059", FontWeight.MEDIUM, 40);
-
-        Text newgame = new Text("NEW GAME");
-        newgame.setFill(Color.WHITE);
-        newgame.setFont(f1);
-        newgame.setOnMouseEntered(event -> newgame.setFill(Color.RED));
-        newgame.setOnMouseExited(event -> newgame.setFill(Color.WHITE));
+        initMainMenu();
         
-        Text highscore = new Text("HIGH SCORES");
-        highscore.setFill(Color.WHITE);
-        highscore.setFont(f1);
-        highscore.setOnMouseEntered(event -> highscore.setFill(Color.RED));
-        highscore.setOnMouseExited(event -> highscore.setFill(Color.WHITE)); 
         
-        Text settings = new Text("SETTINGS");
-        settings.setFill(Color.WHITE);
-        settings.setFont(f1);
-        settings.setOnMouseEntered(event -> settings.setFill(Color.RED));
-        settings.setOnMouseExited(event -> settings.setFill(Color.WHITE)); 
-        
-        Text help = new Text("HELP");
-        help.setFill(Color.WHITE);
-        help.setFont(f1);
-        help.setOnMouseEntered(event -> help.setFill(Color.RED));
-        help.setOnMouseExited(event -> help.setFill(Color.WHITE)); 
-        
-        Text exit = new Text("EXIT");
-        exit.setFill(Color.WHITE);
-        exit.setFont(f1);
-        exit.setOnMouseEntered(event -> exit.setFill(Color.RED));
-        exit.setOnMouseExited(event -> exit.setFill(Color.WHITE));
-        exit.setOnMouseClicked(event -> { 
-        	Node source = (Node)event.getSource();
-        	Stage window = (Stage)source.getScene().getWindow();
-        	window.close();
-        });
-        
-        root.setSpacing(10);
-        // an empty space to push the visible elements up a little
-        Region empty = new Region();
-        empty.setPrefHeight(100);
-        root.getChildren().addAll(newgame, highscore, settings, help, exit, empty);
-		
-        Scene scene = new Scene(root, 680, 640);
-		scene.setFill(Color.BLACK);
-
 		// setup stage
 		stage.setTitle("Asteroids!");
-		stage.setScene(scene);
+		stage.setScene(mainMenu);
 		stage.setOnCloseRequest(event -> Platform.exit());
 		stage.sizeToScene();
 		stage.show();
 		Platform.runLater(() -> stage.setResizable(false));
-		System.out.println(stage.getHeight());
-		System.out.println(stage.getWidth());
-
-        System.out.println(root.getHeight());
-		System.out.println(root.getWidth());
-
 	} // start
+	
+	private void initMainMenu() {
+		
+		Text newGame = new Text("NEW GAME");
+	    newGame.setFill(Color.WHITE);
+	    newGame.setFont(F40);
+	    newGame.setOnMouseEntered(event -> newGame.setFill(Color.RED));
+	    newGame.setOnMouseExited(event -> newGame.setFill(Color.WHITE));
+	    
+	    Text highScore = new Text("HIGH SCORES");
+	    highScore.setFill(Color.WHITE);
+	    highScore.setFont(F40);
+	    highScore.setOnMouseEntered(event -> highScore.setFill(Color.RED));
+	    highScore.setOnMouseExited(event -> highScore.setFill(Color.WHITE)); 
+	    
+	    Text settings = new Text("SETTINGS");
+	    settings.setFill(Color.WHITE);
+	    settings.setFont(F40);
+	    settings.setOnMouseEntered(event -> settings.setFill(Color.RED));
+	    settings.setOnMouseExited(event -> settings.setFill(Color.WHITE)); 
+	    
+	    Text help = new Text("HELP");
+	    help.setFill(Color.WHITE);
+	    help.setFont(F40);
+	    help.setOnMouseEntered(event -> help.setFill(Color.RED));
+	    help.setOnMouseExited(event -> help.setFill(Color.WHITE)); 
+	    
+	    Text exit = new Text("EXIT");
+	    exit.setFill(Color.WHITE);
+	    exit.setFont(F40);
+	    exit.setOnMouseEntered(event -> exit.setFill(Color.RED));
+	    exit.setOnMouseExited(event -> exit.setFill(Color.WHITE));
+	    exit.setOnMouseClicked(event -> { 
+	    	Node source = (Node)event.getSource();
+	    	Stage window = (Stage)source.getScene().getWindow();
+	    	window.close();
+	    });
+	    // an empty space to push the visible elements up a little
+        Region empty = new Region();
+        empty.setPrefHeight(100);
+        
+        VBox root = new VBox();
+		root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+        root.getChildren().addAll(newGame, highScore, settings, help, exit, empty);
+		
+        mainMenu = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        mainMenu.setFill(Color.BLACK);
+	}
 
 } // OmegaApp
