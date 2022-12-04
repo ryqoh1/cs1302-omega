45082db86e42ccd85d879d00bf0fb8fb29e2ce0f
Nhan Thanh Tran <ntt21786@csci-odin.cs.uga.edu>
2022-12-04 11:23:33 -0500

    basic movement

--- src/main/java/cs1302/game/DemoGame.java
+++ src/main/java/cs1302/game/DemoGame.java
@@ -3,9 +3,10 @@ package cs1302.game;
 import java.util.Random;
 import java.util.logging.Level;
 
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Region;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 /**
@@ -16,6 +17,8 @@ public class DemoGame extends Game {
 
     private Random rng;       // random number generator
     private Rectangle player; // some rectangle to represent the player
+    private Image shipon = new Image("file:resources/game/ship_r_on.png");
+    private Image shipoff = new Image("file:resources/game/ship_r.png");
 
 
     /**
@@ -27,39 +30,32 @@ public class DemoGame extends Game {
         super(width, height, 60);            // call parent constructor
         setLogLevel(Level.INFO);             // enable logging
         this.rng = new Random();             // random number generator
-        this.player = new Rectangle(20, 20); // some rectangle to represent the player
+        this.player = new Rectangle(30, 30); // some rectangle to represent the player
 
     } // DemoGame
 
     /** {@inheritDoc} */
     @Override
     protected void init() {
+        player.setFill(new ImagePattern(shipoff));
         // setup subgraph for this component
         getChildren().addAll(player);         // add to main container
         // setup player
-        player.setX(50);                           // 50px in the x direction (right)
-        player.setY(50);                           // 50ps in the y direction (down)
-        player.setOnMouseClicked(event -> handleClickPlayer(event));
+        player.setX(0);
+        player.setY(0);
     } // init
 
     /** {@inheritDoc} */
     @Override
     protected void update() {
 
-        // (x, y)         In computer graphics, coordinates along an x-axis and
-        // (0, 0) -x--->  y-axis are used. When compared to the standard
-        // |              Cartesian plane that most students are familiar with,
-        // y              the x-axis behaves the same, but the y-axis increases
-        // |              in the downward direction! Keep this in mind when
-        // v              adjusting the x and y positions of child nodes.
-
         // update player position
         isKeyPressed( KeyCode.LEFT, () -> player.setX(player.getX() - 10.0));
         isKeyPressed(KeyCode.RIGHT, () -> player.setX(player.getX() + 10.0));
 
-        // <--------------------------------------------------------------------
-        // try adding the code to make the player move up and down!
-        // <--------------------------------------------------------------------
+        // update player position
+        isKeyPressed( KeyCode.UP, () -> player.setY(player.getY() - 10.0));
+        isKeyPressed(KeyCode.DOWN, () -> player.setY(player.getY() + 10.0));
 
     } // update
 
